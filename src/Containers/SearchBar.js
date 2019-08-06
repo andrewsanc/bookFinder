d9642bd682948195f14bfd5d52bb876451814a25
TB831 <druu831@gmail.com>
2019-08-06 11:48:52 -0700

    SearchBar wired up and styled

--- src/Containers/SearchBar.js
+++ src/Containers/SearchBar.js
@@ -4,13 +4,24 @@ class SearchBar extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      term: ''
     }
   }
+
+  onFormSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target.value)
+  }
+
   render() { 
     return (
-      <div>
-        <h1>Search Bar!</h1>
+      <div className="container">
+        <form>
+          <h5>Book Search</h5>
+          <div className="input-field">
+            <input onChange={(e) => this.onFormSubmit(e)} type="text"/>
+          </div>
+        </form>
       </div>
     );
   }
