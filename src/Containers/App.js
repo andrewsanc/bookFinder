7c7e07edec18ba2781cf58eb22026e0fa0388001
TB831 <druu831@gmail.com>
2019-08-06 14:37:39 -0700

    added axios for http library. fetchs data and stores response in state

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
 import SearchBar from './SearchBar';
+import axios from 'axios';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
       bookList: []
     }
   }
 
   onSearchSubmit = (term) => {
-    console.log(term);
+    axios.get('http://openlibrary.org/search.json?', {
+      params: {
+        q: term
+      }
+    })
+      .then(res => this.setState({ bookList: res.data.docs }))
+      .catch(err => console.log(err))
   }
   
   render() {
