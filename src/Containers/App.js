c844b018cea5b4a542505b3446364514f8de82a0
TB831 <druu831@gmail.com>
2019-08-06 11:33:05 -0700

    Constructed SearchBar component boilerplate

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import SearchBar from './SearchBar';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello World!</h1>
+        <SearchBar />
       </div>
     )
   }
