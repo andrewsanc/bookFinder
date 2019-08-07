eba5404e9186233f1a317b9740b686130dc9a58c
TB831 <druu831@gmail.com>
2019-08-07 16:18:54 -0700

    Passing down reducer dummy data down to children components

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,16 +1,10 @@
 import React, { Component } from 'react';
 import SearchBar from './SearchBar';
-import BookList from '../Components/BookList'
+import BookList from '../Components/BookList';
+import { connect } from 'react-redux';
 import axios from 'axios';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      bookList: []
-    }
-  }
-
   onSearchSubmit = (term) => {
     axios.get('http://openlibrary.org/search.json?', {
       params: {
@@ -25,10 +19,16 @@ class App extends Component {
     return (
       <div>
         <SearchBar onSubmit={this.onSearchSubmit} />
-        <BookList bookList={this.state.bookList} />
+        <BookList bookList={this.props.bookList} />
       </div>
     )
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    bookList: state.bookList
+  }
+}
+
+export default connect(mapStateToProps)(App);
