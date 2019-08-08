d7677caffe14f4f4f8c8dc751a3530367a6d1260
TB831 <druu831@gmail.com>
2019-08-08 12:20:13 -0700

    destructuring BookCard props

--- src/Components/BookCard.js
+++ src/Components/BookCard.js
@@ -1,15 +1,15 @@
 // File is in charge of rendering individual cards w/ conditional rendering on line 12.
 import React from 'react';
 
-const BookCard = (props) => {
+const BookCard = ({ title, author }) => {
   return (
     <div className="col s6 m3" >
       <div className="card small teal">
         <div className="card-content">
-          <span className="white-text"><strong>Title:</strong> {props.title}</span>
+          <span className="white-text"><strong>Title:</strong> {title}</span>
           <br />
           <br />
-          {props.author && <span className="white-text"><strong>by</strong> {props.author}</span>}
+          {author && <span className="white-text"><strong>by</strong> {author}</span>}
         </div>
       </div>
     </div>
